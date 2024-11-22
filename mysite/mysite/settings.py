bc8c377c4caf3a2cf64ae205e91341ed85dd05e6
MaksimBritsin <111296865+MaksimBritsin@users.noreply.github.com>
2024-11-22 16:15:53 +0300

    Day 3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = 'ded.maksim999@yandex.ru'
+EMAIL_HOST_PASSWORD = 'bswukxrtkpwbsglz'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
